02431a5fcdcff6e97c50e56b676e15d993e00ac7
Dojiwon <90430566+Dojiwon@users.noreply.github.com>
2024-05-08 20:08:06 +0900

    Add files via upload

---
+++ Week09/iteratorUtilities/iteratorUtilities.cpp
@@ -0,0 +1,50 @@
+﻿// iteratorUtilities.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <iterator>
+#include <unordered_map>
+
+int main()
+{
+    std::unordered_map < std::string, int> myMap{
+        {"Rainer",1966}, {"Beatrix",1966}, {"Juliette",1997}, {"Marius",1999} };
+    for (auto m : myMap) std::cout << "{" << m.first << "," << m.second << "}";
+
+    auto mapItBegin = std::begin(myMap);
+    std::cout << "{" << mapItBegin->first << "," << mapItBegin->second << "}" <<
+        std::endl;
+
+    auto mapIt = std::next(mapItBegin);
+    std::cout << "{" << mapIt->first << ", " << mapIt->second << "}" << std::endl;
+
+    auto dist = std::distance(mapItBegin, mapIt);
+    std::cout << "std::distance(mapItBegin, mapIt): " << dist << std::endl; 
+
+    std::array<int, 10> myArr{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    for (auto a : myArr) std::cout << a << " ";
+
+    auto arrItEnd = std::end(myArr);
+    auto arrIt = std::prev(arrItEnd);
+    std::cout << *arrIt << std::endl;
+
+    std::advance(arrIt, -5);
+    std::cout << *arrIt << std::endl;
+
+    return 0;
+
+
+
+
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
